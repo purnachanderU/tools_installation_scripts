5dee461e6f0315a0eb7796aab54f2d1fa363276d
purnachanderU <31680274+purnachanderU@users.noreply.github.com>
2026-06-13 23:50:57 +0530

    Create script_shell_python.sh

---
+++ script_shell_python.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+str="kratrashi"
+rev=""
+for ((i=${#str}-1; i>=0; i--))
+do
+        rev="${rev}${str:$i:1}"
+done
+echo "$rev"
+
+#######single line#####
+echo "Kar.manchkki" | awk -F'.' '{print $1; print $2; print "Count of a:", gsub(/[aA]/,"&")}'
+
+#########
+s = "Kar.manchi"
+
+rev = ""
+
+for ch in s:
+    rev = ch + rev
+print(rev)
+
+################
+s = "Kar.manchi"
+
+print(s[::-1])
+
+
+
