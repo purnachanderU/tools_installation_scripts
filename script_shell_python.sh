d0f54f09c7d52ece4282bc6f21807a57f41e6087
purnachanderU <31680274+purnachanderU@users.noreply.github.com>
2026-06-19 00:05:52 +0530

    Enhance string manipulation with subprocess and examples
    Added subprocess command to split string and print parts. Included examples of reversing strings using slicing and a for loop.

--- script_shell_python.sh
+++ script_shell_python.sh
@@ -32,9 +32,26 @@ for ch in str:
     rev = ch + rev
 print(rev)
 
-####################
+########################################################
 st = "hello.devops"
 
 parts = st.split(".")
 print(parts[0])
 print(parts[1])
+
+##########import subprocess #################################
+import subprocess
+st = "hello.devops"
+cmd = f"echo '{st}' | awk -F'.' '{{print $1; print $2}}'"
+output = subprocess.check_output(cmd, shell=True, text=True)
+print(output)
+# Reverse using slicing
+s = "devops.batch"
+print("Reverse:", s[::-1])
+# Reverse using for loop
+st = "forloop.batch15"
+ch = ""
+for i in st:
+    ch = i + ch
+print("Reverse using loop:", ch)
+######################################################
